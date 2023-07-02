e1189522be9edbaaedd5a4cbefd35d1a7fa07501
Sangam-Yadav-au51 <112935388+Sangam-Yadav-au51@users.noreply.github.com>
2023-07-02 09:20:07 -0700

    vercel changing

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-import colors from 'colors'
 import userRoutes from "./src/routes/Users.js";
 import questionRoutes from "./src/routes/Questions.js";
 import answerRoutes from "./src/routes/Answers.js";
@@ -9,7 +7,7 @@ import chatbotRoutes from "./src/routes/Chatbot.js";
 import connectDB from './src/db/connectDB.js'
 import otpRoutes from './src/routes/Otp.js'
   
-dotenv.config()
+
 connectDB()
 
 const PORT = process.env.PORT || 5000
@@ -27,7 +25,7 @@ app.use('/otp', otpRoutes)
 
 
 app.get('/', (req, res) => {
-  res.send("This is a stack overflow clone's API by Gautam Jha")
+  res.send("This is a stack overflow clone's API")
 })
 
 app.listen(PORT, () => {
